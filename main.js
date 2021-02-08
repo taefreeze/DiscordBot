83046498a396d02bb046ba11ae644b7d82e672c9
taefreeze <potaekingdom@hotmail.com>
2021-02-08 10:18:49 +0700

    Ping Pong message

--- main.js
+++ main.js
@@ -2,8 +2,21 @@ const Discord = require('discord.js');
 
 const client = new Discord.Client();
 
-client.once('ready',() => {
+const prefix = '-';
+
+client.once('ready', () => {
     console.log('Online');
 });
 
-client.login('ODA4MTQzMTYzOTcyNzgwMDQy.YCCP-A.Gr0VgxQ2cE-4thNvZA1bsB-ikgI');
+client.on('message', message => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if (command === 'ping') {
+        message.channel.send('Pong!');
+    }
+});
+
+client.login(TokenId);

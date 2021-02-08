3cdb06007543097fc2c37b4ff9d520afecee17fb
TaeFreeze <potaekingdom@hotmail.com>
2021-02-08 08:25:40 +0700

    Add Console.log message

---
+++ main.js
@@ -0,0 +1,9 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+client.once('ready',() => {
+    console.log('Online');
+});
+
+client.login('ODA4MTQzMTYzOTcyNzgwMDQy.YCCP-A.Gr0VgxQ2cE-4thNvZA1bsB-ikgI');
